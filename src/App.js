4347a883b7b29b21e57bfd4a21f2e6baa8b5e416
Brad Aagaard <baagaard@usgs.gov>
2017-01-11 16:06:50 -0800

    Implemented version information.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './App.css';
 
 // :TODO:
+// Refresh button
 // Version
 // Typecheck with PropTypes
 // Can only update a mounted or mounting component. This usually means you called setState() on an unmounted component. This is a no-op. Please check the code for the ComponentAvatar component.
@@ -237,12 +238,138 @@ class DetailPanel extends React.Component {
     } // render
 } // DetailPanel
 
-class Version extends React.Component {
+class VersionSimple extends React.Component {
     render() {
 	return (
+		<div className="version-package">
+		<h3>{this.props.name}</h3>
 		<dl>
-		<dt>PyLith</dt><dd>{this.props.version.pylith}</dd>
+		<dt>Version</dt><dd><span className="version">{this.props.value.version}</span></dd>
 		</dl>
+		</div>
+	);
+    } // render
+} // VersionSimple
+
+class VersionGit extends React.Component {
+    render() {
+	if (this.props.value.isRelease) {
+	    return (
+		    <div className="version-package">
+		    <h3>{this.props.name}</h3>
+		    <dl>
+		    <dt>Release</dt><dd><span className="version">v{this.props.value.version}</span></dd>
+		    </dl>
+		    </div>
+	    );
+	} else {
+	    return (
+		    <div className="version-package">
+		    <h3>{this.props.name}</h3>
+		    <dl>
+		    <dt>Configured</dt><dd>{this.props.value.gitDate}</dd>
+		    <dt>GIT branch</dt><dd>{this.props.value.gitBranch}</dd>
+		    <dt>GIT revision</dt><dd><span className="version">{this.props.value.gitRevision}</span></dd>
+		    <dt>GIT hash</dt><dd>{this.props.value.gitHash}</dd>
+		    </dl>
+		    </div>
+	    );
+	} // if/else
+    } // render
+} // VersionGit
+
+class VersionPetsc extends React.Component {
+    render() {
+	if (this.props.value.isRelease) {
+	    return (
+		    <div className="version-package">
+		    <h3>PETSc</h3>
+		    <dl>
+		    <dt>Release</dt><dd><span className="version">v{this.props.value.version}</span></dd>
+		    <dt>PETSC_DIR</dt><dd><span className="file-path">{this.props.value.petscDir}</span></dd>
+		    <dt>PETSC_ARCH</dt><dd>{this.props.value.petscArch}</dd>
+		    </dl>
+		    </div>
+	    );
+	} else {
+	    return (
+		    <div className="version-package">
+		    <h3>PETSc</h3>
+		    <dl>
+		    <dt>Configured</dt><dd>{this.props.value.gitDate}</dd>
+		    <dt>GIT branch</dt><dd>{this.props.value.gitBranch}</dd>
+		    <dt>GIT revision</dt><dd><span className="version">{this.props.value.gitRevision}</span></dd>
+		    <dt>PETSC_DIR</dt><dd><span className="file-path">{this.props.value.petscDir}</span></dd>
+		    <dt>PETSC_ARCH</dt><dd>{this.props.value.petscArch}</dd>
+		    </dl>
+		    </div>
+	    );
+	} // if/else
+    } // render
+} // VersionPetsc
+
+class VersionMPI extends React.Component {
+    render() {
+	return (
+		<div className="version-package">
+		<h3>MPI</h3>
+		<dl>
+		<dt>Standard</dt><dd>{this.props.value.standard}</dd>
+		<dt>Implementation</dt><dd>{this.props.value.implementation}</dd>
+		<dt>Version</dt><dd><span className="version">{this.props.value.version}</span></dd>
+		</dl>
+		</div>
+	);
+    } // render
+} // VersionMPI
+
+class VersionPython extends React.Component {
+    render() {
+	const modules = Object.keys(this.props.value.modules).map((name) => {
+		const module = this.props.value.modules[name];
+		return (
+		    <span key={name}><dt>{name}</dt><dd><span className="version">v{module.version}</span> from <span className="file-path">{module.location}</span></dd></span>
+		);
+	    }, this);
+	return (
+		<div className="version-package">
+		<h3>Python</h3>
+		<dl>
+		<dt>Version</dt><dd><span className="version">{this.props.value.version}</span></dd>
+		<dt>Compiled with</dt><dd>{this.props.value.compiler}</dd>
+		<dt>Executable</dt><dd><span className="file-path">{this.props.value.executable}</span></dd>
+		<dt>Modules</dt><dd>
+		<dl>{modules}</dl>
+		</dd>
+		</dl>
+		</div>
+	);
+    } // render
+} // VersionMPI
+
+class Version extends React.Component {
+    render() {
+	if (!this.props.version) {
+	    return null;
+	} // if
+
+	const pylith = this.props.version.pylith;
+	const dependencies = this.props.version.dependencies;
+
+	// :TODO:
+
+	return (
+		<div className="version">
+		<VersionGit name="PyLith" value={pylith} />
+		<h2>Dependencies</h2>
+		<VersionPetsc value={dependencies.petsc} />
+		<VersionMPI value={dependencies.mpi} />
+		<VersionGit name="Spatialdata" value={dependencies.spatialdata} />
+		<VersionSimple name="Proj.4" value={dependencies.proj4} />
+		<VersionSimple name="HDF5" value={dependencies.hdf5} />
+		<VersionSimple name="NetCDF" value={dependencies.netcdf} />
+		<VersionPython value={dependencies.python} /> 
+	    </div>
 	);
     } // render
 } // Version
